6eb4fa1c0a2f899713a12f6df2e50cb3024f4eec
Flxbot <92648520+FlxBot001@users.noreply.github.com>
2024-12-12 21:51:24 +0300

    Changes

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { gridItems } from '@/data'
 
 const Grid = () => {
   return (
-    <div>i</div>
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map(({ id, title, description, className, img, imgClassName, titleClassName, spareImg }) => (
+          <BentoGridItem 
+            id={id}
+            key={id}
+            title={title}
+            description={description}
+            className={className}
+            img={img}
+            imgClassname={imgClassName}
+            titleClassName={titleClassName}
+            spareImg={imgClassName}
+          />
+        ))}
+      </BentoGrid>
+    </section>
   )
 }
 
